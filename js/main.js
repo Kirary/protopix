c8bbbb51a820bff3858654d2b75e5b3e7fea4437
Kirill Zorin <zskwa7@gmail.com>
2023-03-25 14:50:27 +0500

    pew

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 vkBridge.send("VKWebAppInit");
+
+const button = document.getElementById("yellow-btn");
+
+const sound = new Howl({
+  src: [require("../sounds/pew.mp3")],
+});
+
+sound.onload = () => {
+  console.log("loaded");
+};
+
+button.onclick = () => {
+  button.classList.add("px-8");
+  button.classList.add("py-12");
+  button.classList.add("text-2xl");
+
+  sound.play();
+
+  setTimeout(() => {
+    button.classList.remove("px-8");
+    button.classList.remove("py-12");
+    button.classList.remove("text-2xl");
+  }, 200);
+};
+
+setInterval(() => {
+  console.log(sound.state());
+}, 100);
